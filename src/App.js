2bc8bd8173fa11b2d09622764a230784d626e764
ConnorHoare <connor@tipcigroup.com>
2023-02-05 10:03:52 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchForm from './components/SearchForm';
+import Navbar from './components/Nabar';
+import Photo from './components/Photo';
 
 function App() {
   return (
     <div className="App">
         <SearchForm />
+        <Navbar />
+        <Photo />
     </div>
   );
 }
